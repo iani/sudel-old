271c89521b42f0c6ebc54ca079ac4fe45a1ddf64
neyzen <zannos@gmail.com>
2015-03-15 11:34:48 +0200

    Updated on: Sun Mar 15 11:34:48 EET 2015

--- publish.sh
+++ publish.sh
@@ -1,5 +1,6 @@
 org-export html --infile sudel.org --outfile index.html
 git checkout master
+cp ~/Dropbox/000WORKFILES/0_PRIVATE/SUDEL.org ./
 git add --all .
 git commit -am "Updated on: `date`"
 git push origin master
